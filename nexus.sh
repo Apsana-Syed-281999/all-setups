3623e285515f13fe7338bbae214abd620fb545a9
Apsana-Syed-281999 <syedapsana281999@gmail.com>
2026-07-15 13:10:26 +0530

    Update nexus.sh for Nexus installation steps

--- nexus.sh
+++ nexus.sh
@@ -4,7 +4,6 @@ sudo yum install java-21-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
 sudo wget https://download.sonatype.com/nexus/3/nexus-3.94.0-12-linux-x86_64.tar.gz
 sudo tar -xvf nexus-3.94.0-12-linux-x86_64.tar.gz
-sudo mv nexus-3.94.0-04 nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/
 sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
