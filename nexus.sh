caa0f990903143416741babb92f0a068871bb97e
Apsana-Syed-281999 <syedapsana281999@gmail.com>
2026-07-15 12:59:23 +0530

    Update Nexus download to version 3.94.0-12

--- nexus.sh
+++ nexus.sh
@@ -2,8 +2,8 @@ sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-21-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.94.0-04-linux-x86_64.tar.gz
-sudo tar -xvf nexus-3.94.0-04-linux-x86_64.tar.gz
+sudo wget https://download.sonatype.com/nexus/3/nexus-3.94.0-12-linux-x86_64.tar.gz
+sudo tar -xvf nexus-3.94.0-12-linux-x86_64.tar.gz
 sudo mv nexus-3.94.0-04 nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
