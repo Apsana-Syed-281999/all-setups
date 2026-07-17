9ad96ce1ed4248f58765fca0799ea766b1369c31
Apsana-Syed-281999 <syedapsana281999@gmail.com>
2026-07-17 18:12:30 +0530

    Add commands to switch to nexus user and navigate to app

--- nexus.sh
+++ nexus.sh
@@ -6,3 +6,5 @@ sudo wget https://download.sonatype.com/nexus/3/nexus-3.94.0-12-linux-x86_64.tar
 sudo tar -xvf nexus-3.94.0-12-linux-x86_64.tar.gz
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/
+su - nexus
+cd /app/
